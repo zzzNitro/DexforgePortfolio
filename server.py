17d328648e73d4fbed3a8b12db0e99e1912a7d5d
Cata <cataurruilab@gmail.com>
2020-08-28 21:56:09 -0400

    form

--- server.py
+++ server.py
@@ -8,3 +8,16 @@ def pablo_home():
     return render_template('index.html')
 
 
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_contact', methods=['POST', 'GET'])
+def submit_contact():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/gracias.html')
+    else:
+        return 'Hubo un problema, intenta de nuevo.'
