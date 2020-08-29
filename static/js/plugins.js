652551756d316ade0c16448d0b7cd4f3bae9e9fe
zzzNitro <pablo.benito@rocketmail.com>
2020-08-29 00:48:37 -0400

    Revert "form"
    This reverts commit 17d32864

--- static/js/plugins.js
+++ static/js/plugins.js
@@ -36,36 +36,6 @@
     this.option(options);
   }
 
-  // PRUEBA MODAL
-
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-
-  // FIN PRUEBA MODAL
-
   // Descriptions of all options available on the demo site:
   // http://lokeshdhakar.com/projects/lightbox2/index.html#options
   Lightbox.defaults = {
